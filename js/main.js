901c67c0347b30ab6f7ebf5ade4acde71389c6d2
FakhriddinovSh <shaxobiddinfaxriddinov@yandex.com>
2022-04-27 22:11:42 +0500

    Initial

--- js/main.js
+++ js/main.js
@@ -8,16 +8,16 @@ const animated_bgs = document.querySelector(".animated-bg")
 const animated_bg_texts = document.querySelector('.animated-bg-text');
 
 
-setTimeout(getData, 2500)
-function getData() {
-    header.innerHTML = "";
-    'img src="https://source.unsplash.com/1600x900/?naturemwater"/>'
-    title.innerHTML = 'google codewithrandom'
-    excerpt.innerHTML = "okopkpokpokpokpokpok"
-    profile_img.innerHTML = 
-    'img src="https://source.unsplash.com/1600x900/?naturemwater" alt="" />'
-    names.innerHTML = "CODEWITHRANDOM"
-    date.innerHTML = "22jan 2021"
-    animated_bgs.forEach((bg)=> bg.classList.remove('animated-bg'));
-    animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
-};  
+// setTimeout(getData, 2500)
+// function getData() {
+//     header.innerHTML = "";
+//     'img src="https://source.unsplash.com/1600x900/?naturemwater"/>'
+//     title.innerHTML = 'google codewithrandom'
+//     excerpt.innerHTML = "okopkpokpokpokpokpok"
+//     profile_img.innerHTML = 
+//     'img src="https://source.unsplash.com/1600x900/?naturemwater" alt="" />'
+//     names.innerHTML = "CODEWITHRANDOM"
+//     date.innerHTML = "22jan 2021"
+//     animated_bgs.forEach((bg)=> bg.classList.remove('animated-bg'));
+//     animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+// };  
